420257a402b20c57426d52ad550115ffd68bb8c5
Jordan Ross <jordan@jordanross.net>
2019-10-31 10:57:53 -0700

    asd

--- examples/demo/showcase/main.py
+++ examples/demo/showcase/main.py
@@ -59,12 +59,12 @@ class ShowcaseApp(App):
         self.title = 'hello world'
         Clock.schedule_interval(self._update_clock, 1 / 60.)
         self.screens = {}
-        self.available_screens = sorted([
+        self.available_screens = [
             'Buttons', 'ToggleButton', 'Sliders', 'ProgressBar', 'Switches',
             'CheckBoxes', 'TextInputs', 'Accordions', 'FileChoosers',
             'Carousel', 'Bubbles', 'CodeInput', 'DropDown', 'Spinner',
             'Scatter', 'Splitter', 'TabbedPanel + Layouts', 'RstDocument',
-            'Popups', 'ScreenManager'])
+            'Popups', 'ScreenManager']
         self.screen_names = self.available_screens
         curdir = dirname(__file__)
         self.available_screens = [join(curdir, 'data', 'screens',
